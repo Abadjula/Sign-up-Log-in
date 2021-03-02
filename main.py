a5900dcaf44c9510f940b46d44aa1841cab93cb7
Abadjula <76713140+Abadjula@users.noreply.github.com>
2021-03-01 19:10:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 running = True
+# Loops forever
 while running:
+# If he says yes it will keep on going if he says no it will cancel if he says somthing else it would print error
     Check_if_want_sign_up = input("Hello welcome to this sign up page to sign up type yes to cancel type no: ")
     if Check_if_want_sign_up == "no":
         print("Bye bye hopefully we see you again")
@@ -18,11 +20,14 @@ while running:
             Tries += 1
             Username = input("Enter your username: ")
             Password = input("Enter your password: ")
+# If he gets it right the loop will end and will say hello to the user
             if Username == Sign_up_username and Password == Sign_up_password:
                 print("Hello " + Username + " you successfully signed up")
                 running = False
+# If he gets it wrong it will show how many attempts he has and will cancel if he gets it wrong in less than 5 tries
             elif Username != Sign_up_username or Password != Sign_up_password:
                 Show_tries -= 1
                 print("You have " + str(Show_tries) + " attempts left")
     else:
         print("Error")
+# Made by Abadjula#4856
